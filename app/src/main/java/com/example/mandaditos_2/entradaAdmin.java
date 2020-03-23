e8fbcb435086d80328cc908b19042a4cd753620e
Sahagun Ortega Noe Fermin <39476935+DopeXale@users.noreply.github.com>
2020-03-23 16:51:00 -0600

    opcion registro de negocios

--- app/src/main/java/com/example/mandaditos_2/entradaAdmin.java
+++ app/src/main/java/com/example/mandaditos_2/entradaAdmin.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,35 +29,18 @@ public class entradaAdmin extends AppCompatActivity {
         Button entrar = findViewById(R.id.btnAdmin);
 
         EditText datos = findViewById(R.id.edtPassword);
+
+
         final String editPass = datos.getText().toString();
 
         entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ref = FirebaseDatabase.getInstance().getReference().child("Users").child("id1");
-                ref.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                        String firebasePass = dataSnapshot.child("pass").getValue().toString();
-
-                        if (firebasePass.equals(editPass)){
-                            Intent i = new Intent(entradaAdmin.this,MenuAdmin.class);
-                            startActivity(i);
-                        }else {
-                            Intent i = new Intent(entradaAdmin.this,MainActivity.class);
-                            startActivity(i);
-                        }
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
+                Intent i = new Intent(entradaAdmin.this,MenuAdmin.class);
+                startActivity(i);
             }
         });
+
 
     }
 
