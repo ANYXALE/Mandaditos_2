5d604220f8a64de2ab8682b819260e51a6550056
Sahagun Ortega Noe Fermin <39476935+DopeXale@users.noreply.github.com>
2020-03-22 20:26:21 -0600

    boton que manda a pestaña de ingreso admin

---
+++ app/src/main/java/com/example/mandaditos_2/entradaAdmin.java
@@ -0,0 +1,14 @@
+package com.example.mandaditos_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class entradaAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrada_admin);
+    }
+}
