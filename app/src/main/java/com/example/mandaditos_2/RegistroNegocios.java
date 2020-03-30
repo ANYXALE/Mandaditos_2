e1d4f0f2d386cde156c7c8b77d12d4797c84be75
anyxale <annel.hernandez@alumnos.udg.mx>
2020-03-29 22:16:48 -0600

    ya la cague no sirve nada

--- app/src/main/java/com/example/mandaditos_2/RegistroNegocios.java
+++ app/src/main/java/com/example/mandaditos_2/RegistroNegocios.java
@@ -1,22 +1,49 @@
 package com.example.mandaditos_2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.storage.StorageReference;
 
 public class RegistroNegocios extends AppCompatActivity {
+private StorageReference cloud;
 
 
-
+    ImageView imanegocio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_negocios);
         getSupportActionBar().hide();
 
+        imanegocio =findViewById(R.id.imageNegocio);
+    }
 
+    public void onclick(View view) {
+        cargarImagen();
+    }
+
+    private void cargarImagen() {
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"seleccione la imagen"),10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode==RESULT_OK){
+            Uri path=data.getData();
+            imanegocio.setImageURI(path);
+        }
     }
 }
